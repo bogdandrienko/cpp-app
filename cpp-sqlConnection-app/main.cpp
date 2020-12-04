e1a5a678963ab507e41987a108b656be83654318
Bogdan <67187037+bogdandrienko@users.noreply.github.com>
2020-12-04 08:04:16 +0600

    update

---
+++ cpp-sqlConnection-app/main.cpp
@@ -0,0 +1,111 @@
+﻿// cpp-sqlConnection-app.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+#include "main.h"
+
+//use the std namespace
+using namespace std;
+
+int main() {
+#define SQL_RESULT_LEN 240
+#define SQL_RETURN_CODE_LEN 1000
+    //define handles and variables
+    SQLHANDLE sqlConnHandle;
+    SQLHANDLE sqlStmtHandle;
+    SQLHANDLE sqlEnvHandle;
+    SQLWCHAR retconstring[SQL_RETURN_CODE_LEN];
+    //initializations
+    sqlConnHandle = NULL;
+    sqlStmtHandle = NULL;
+    //allocations
+    if (SQL_SUCCESS != SQLAllocHandle(SQL_HANDLE_ENV, SQL_NULL_HANDLE, &sqlEnvHandle))
+        goto COMPLETED;
+    if (SQL_SUCCESS != SQLSetEnvAttr(sqlEnvHandle, SQL_ATTR_ODBC_VERSION, (SQLPOINTER)SQL_OV_ODBC3, 0))
+        goto COMPLETED;
+    if (SQL_SUCCESS != SQLAllocHandle(SQL_HANDLE_DBC, sqlEnvHandle, &sqlConnHandle))
+        goto COMPLETED;
+    //output
+    cout << "Attempting connection to SQL Server...";
+    cout << "\n";
+    //connect to SQL Server  
+    //I am using a trusted connection and port 14808
+    //it does not matter if you are using default or named instance
+    //just make sure you define the server name and the port
+    //You have the option to use a username/password instead of a trusted connection
+    //but is more secure to use a trusted connection
+    switch (SQLDriverConnect(sqlConnHandle,
+        NULL,
+        (SQLWCHAR*)L"DRIVER={SQL Server};SERVER=localhost, 14808;DATABASE=MS_SQL;UID=SQL_Server;PWD=31284bogdan;",
+        //(SQLWCHAR*)L"DRIVER={SQL Server};SERVER=localhost, 14808;DATABASE=master;Trusted=true;",
+        //(SQLWCHAR*)L"DRIVER={SQL Server};SERVER=localhost, 14808;DATABASE=MSSQL;Trusted=true;",
+        SQL_NTS,
+        retconstring,
+        1024,
+        NULL,
+        SQL_DRIVER_NOPROMPT)) {
+    case SQL_SUCCESS:
+        cout << "Successfully connected to SQL Server";
+        cout << "\n";
+        break;
+    case SQL_SUCCESS_WITH_INFO:
+        cout << "Successfully connected to SQL Server";
+        cout << "\n";
+        break;
+    case SQL_INVALID_HANDLE:
+        cout << "Could not connect to SQL Server";
+        cout << "\n";
+        goto COMPLETED;
+    case SQL_ERROR:
+        cout << "Could not connect to SQL Server";
+        cout << "\n";
+        goto COMPLETED;
+    default:
+        break;
+    }
+    //if there is a problem connecting then exit application
+    if (SQL_SUCCESS != SQLAllocHandle(SQL_HANDLE_STMT, sqlConnHandle, &sqlStmtHandle))
+        goto COMPLETED;
+    //output
+    cout << "\n";
+    cout << "Executing T-SQL query...";
+    cout << "\n";
+    //if there is a problem executing the query then exit application
+    //else display query result
+    if (SQL_SUCCESS != SQLExecDirect(sqlStmtHandle, (SQLWCHAR*)L"SELECT @@VERSION", SQL_NTS)) {
+        cout << "Error querying SQL Server";
+        cout << "\n";
+        goto COMPLETED;
+    }
+    else {
+        //declare output variable and pointer
+        SQLCHAR sqlVersion[SQL_RESULT_LEN];
+        SQLINTEGER ptrSqlVersion;
+        while (SQLFetch(sqlStmtHandle) == SQL_SUCCESS) {
+            SQLGetData(sqlStmtHandle, 1, SQL_CHAR, sqlVersion, SQL_RESULT_LEN, &ptrSqlVersion);
+            //display query result
+            cout << "\nQuery Result:\n\n";
+            cout << sqlVersion << endl;
+        }
+    }
+    //close connection and free resources
+COMPLETED:
+    SQLFreeHandle(SQL_HANDLE_STMT, sqlStmtHandle);
+    SQLDisconnect(sqlConnHandle);
+    SQLFreeHandle(SQL_HANDLE_DBC, sqlConnHandle);
+    SQLFreeHandle(SQL_HANDLE_ENV, sqlEnvHandle);
+    //pause the console window - exit when key is pressed
+    cout << "\nPress any key to exit...";
+    getchar();
+}
+
+
+
+// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
+// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
+
+// Советы по началу работы 
+//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
+//   2. В окне Team Explorer можно подключиться к системе управления версиями.
+//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
+//   4. В окне "Список ошибок" можно просматривать ошибки.
+//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
+//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
---
+++ cpp-sqlConnection-app/main.h
@@ -0,0 +1,8 @@
+#pragma once
+
+#include <iostream>
+#include <windows.h>
+#include <sqlext.h>
+#include <sqltypes.h>
+#include <sql.h>
+#include <sql.h>
