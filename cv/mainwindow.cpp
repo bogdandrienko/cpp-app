6857eb06a7483df23132bd74a189064c9156e3fe
Bogdan <67187037+bogdandrienko@users.noreply.github.com>
2021-06-03 07:08:27 +0600

    update

--- cv/mainwindow.cpp
+++ cv/mainwindow.cpp
@@ -11,7 +11,7 @@ MainWindow::MainWindow(QWidget *parent)
     ui->setupUi(this);
 
     // read an image
-    cv::Mat image = cv::imread("C:\\Project\\1.jpg", 1);
+    cv::Mat image = cv::imread("C:\\Project\\Github_Projects\\cpp-app\\ComputerVision", 1);
     // create image window named "My Image"
     cv::namedWindow("My Image");
     // show the image on window
